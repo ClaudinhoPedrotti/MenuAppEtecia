89ecf548a9b0bc6422cd22d7339ffd103e96b925
ClaudinhoPedrotti <claudio.brito9@etec.sp.gov.br>
2019-05-08 22:07:09 -0300

    08/05/2019 2

--- app/src/main/java/br/com/etecia/menuappetecia/MainActivity.java
+++ app/src/main/java/br/com/etecia/menuappetecia/MainActivity.java
@@ -3,8 +3,14 @@ package br.com.etecia.menuappetecia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.idToolBar);
+        toolbar = (Toolbar) findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setIcon(R.drawable.imgflecha);
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mCompartilhar:
+                Toast.makeText(getApplicationContext(), "Compartilhar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mConfigurcoes:
+                Toast.makeText(getApplicationContext(), "Configurações", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mFavoritos:
+                Toast.makeText(getApplicationContext(), "Favoritos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mInformacoes:
+                Toast.makeText(getApplicationContext(), "Informações", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mPesquisar:
+                Toast.makeText(getApplicationContext(), "Pesquisar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mSalvos:
+                Toast.makeText(getApplicationContext(), "Salvos", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
